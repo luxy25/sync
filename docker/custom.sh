7bd0a4686460de648336a2f2d85b4c3ef5ca0ef3
luxiaoyang <luxy25@gmail.com>
2021-09-01 14:49:16 +0800

    Create custom.sh
    提交

---
+++ docker/custom.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "自定义脚本，设置docker仓库git。。。"
+
+cd /jds && git remote set-url origin https://github.com/luxy25/someDockerfile && git remote -vv && git branch --set-upstream-to=origin/master master && cd /scripts 
+
